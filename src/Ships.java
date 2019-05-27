afaa441732f5534e6f9e6b2165a4e0969372fefd
MiriamVar <mimika648@gmail.com>
2019-05-27 12:11:17 +0200

    finished

--- src/Ships.java
+++ src/Ships.java
@@ -41,184 +41,159 @@ public class Ships {
         for (int i = 5; i > 1; i--) {  // ake dlhe su
             for (int j = 6-i; j >= 1; j--) {//kolko ich je
                 int z =0;
-                while(z<3 && !generatingShip(i)){
+                while(!generatingShip(i)){
                     z++;
                 }
             }
         }
-//        for (int i = 1; i < 5; i++) {  // ake dlhe su
-//            for (int j = 5; j >1; j--) {//kolko ich je
-//                generatingShip(j);
-//            }
-//        }
-
-//        generatingShip(5);
-//
-//        generatingShip(4);
-//        generatingShip(4);
-//
-//        generatingShip(3);
-//        generatingShip(3);
-//        generatingShip(3);
-//
-//        generatingShip(2);
-//        generatingShip(2);
-//        generatingShip(2);
-//        generatingShip(2);
-
     }
 
     private boolean generatingShip(int num){
-       Point startP =  startPoint();
-       if (gameboard[startP.getX()][startP.getY()] != 0){
-           return false;
-       }
-       if (gameboard[startP.getX()][startP.getY()] == 0){
-           boolean access = true;
-           
+        Point startP =  startPoint();
+        if (gameboard[startP.getX()][startP.getY()] != 0){
+            return false;
+        }
+        if (gameboard[startP.getX()][startP.getY()] == 0){
+            boolean access = true;
+
 //           kontorlujem smer hore
-           for (int i =1; i<num; i++){
-               if (startP.getY()+i>9){
-                   access = false;
-                   break;
-               }
-               if(gameboard[startP.getX()][startP.getY()+i] != 0){
-                   access = false;
-                   break;
-               }
-           }
-           if(access){
-               for (int i =0; i<num; i++){
-                   gameboard[startP.getX()][startP.getY()+i] = 1;
+            for (int i =1; i<num; i++){
+                if (startP.getY()+i>9){
+                    access = false;
+                    break;
+                }
+                if(gameboard[startP.getX()][startP.getY()+i] != 0){
+                    access = false;
+                    break;
+                }
+            }
+            if(access){
+                for (int i =0; i<num; i++){
+                    gameboard[startP.getX()][startP.getY()+i] = 1;
 //                   boky
-                   if(startP.getX()-1 >=0){
-                       gameboard[startP.getX()-1][startP.getY()+i] = -1;
-                   }
-                   if(startP.getX()+1 <=9){
-                       gameboard[startP.getX()+1][startP.getY()+i] = -1;
-                   }
-               }
-               //                   hore dole
-               if(startP.getY()-1 >=0){
-                   gameboard[startP.getX()][startP.getY()-1] = -1;
-               }
-               if(startP.getY()+num <=9){
-                   gameboard[startP.getX()][startP.getY()+num] = -1;
-               }
-               return true;
-           }
-           access= true;
+                    border(startP.getX()+1,startP.getY()+i);
+                    border(startP.getX()-1,startP.getY()+i);
+                }
+                //                   hore dole
+                border(startP.getX(),startP.getY()-1);
+                border(startP.getX(),startP.getY()+num);
+                // diagonal
+                border(startP.getX()+1,startP.getY()-1);
+                border(startP.getX()+1,startP.getY()+num);
+                border(startP.getX()-1,startP.getY()-1);
+                border(startP.getX()-1,startP.getY()+num);
+                return true;
+            }
+            access= true;
 
 
 
 
 
-           //           kontolujem smer doprava
-           for (int i =1; i<num; i++){
-               if (startP.getX()+i>9){
-                   access = false;
-                   break;
-               }
-               if(gameboard[startP.getX()+i][startP.getY()] != 0){
-                   access = false;
-                   break;
-               }
-           }
-           if(access){
-               for (int i =0; i<num; i++){
-                   gameboard[startP.getX()+i][startP.getY()] = 1;
+            //           kontolujem smer doprava
+            for (int i =1; i<num; i++){
+                if (startP.getX()+i>9){
+                    access = false;
+                    break;
+                }
+                if(gameboard[startP.getX()+i][startP.getY()] != 0){
+                    access = false;
+                    break;
+                }
+            }
+            if(access){
+                for (int i =0; i<num; i++){
+                    gameboard[startP.getX()+i][startP.getY()] = 1;
 //                   boky
-                   if(startP.getY()-1 >=0){
-                       gameboard[startP.getX()+i][startP.getY()-1] = -1;
-                   }
-                   if(startP.getY()+1 <=9){
-                       gameboard[startP.getX()+i][startP.getY()+1] = -1;
-                   }
-               }
-               //                   hore dole
-               if(startP.getX()-1 >=0){
-                   gameboard[startP.getX()-1][startP.getY()] = -1;
-               }
-               if(startP.getX()+num <=9){
-                   gameboard[startP.getX()+num][startP.getY()] = -1;
-               }
-               return true;
-           }
-           access= true;
+                    border(startP.getX()+i,startP.getY()-1);
+                    border(startP.getX()+i,startP.getY()+1);
+                }
+                //                   hore dole
+                border(startP.getX()-1,startP.getY());
+                border(startP.getX()+num,startP.getY());
+                // diagonal
+                border(startP.getX()-1,startP.getY()+1);
+                border(startP.getX()+num,startP.getY()+1);
+                border(startP.getX()-1,startP.getY()-1);
+                border(startP.getX()+num,startP.getY()-1);
+                return true;
+            }
+            access= true;
 
 
 
 
 
 //           kontolujem smer dole
-           for (int i =1; i<num; i++){
-               if (startP.getY()-i<0){
-                   access = false;
-                   break;
-               }
-               if(gameboard[startP.getX()][startP.getY()-i] != 0){
-                   access = false;
-                   break;
-               }
-           }
-           if(access){
-               for (int i =0; i<num; i++){
-                   gameboard[startP.getX()][startP.getY()-i] = 1;
+            for (int i =1; i<num; i++){
+                if (startP.getY()-i<0){
+                    access = false;
+                    break;
+                }
+                if(gameboard[startP.getX()][startP.getY()-i] != 0){
+                    access = false;
+                    break;
+                }
+            }
+            if(access){
+                for (int i =0; i<num; i++){
+                    gameboard[startP.getX()][startP.getY()-i] = 1;
 //                   boky
-                   if(startP.getX()-1 >=0){
-                       gameboard[startP.getX()-1][startP.getY()-i] = -1;
-                   }
-                   if(startP.getX()+1 <=9){
-                       gameboard[startP.getX()+1][startP.getY()-i] = -1;
-                   }
-               }
-               //                   hore dole
-               if(startP.getY()+1 <=9){
-                   gameboard[startP.getX()][startP.getY()+1] = -1;
-               }
-               if(startP.getY()-num >=0){
-                   gameboard[startP.getX()][startP.getY()-num] = -1;
-               }
-               return true;
-           }
-           access= true;
+                    border(startP.getX()+1,startP.getY()-i);
+                    border(startP.getX()-1,startP.getY()-i);
+                }
+                //                   hore dole
+                border(startP.getX(),startP.getY()+1);
+                border(startP.getX(),startP.getY()+num);
+                // diagonal
+                border(startP.getX()+1,startP.getY()+1);
+                border(startP.getX()+1,startP.getY()+num);
+                border(startP.getX()-1,startP.getY()+1);
+                border(startP.getX()-1,startP.getY()+num);
+                return true;
+            }
+            access= true;
 
 
 
-           //           kontolujem smer dolava
-           for (int i =1; i<num; i++){
-               if (startP.getX()-i<0){
-                   access = false;
-                   break;
-               }
-               if(gameboard[startP.getX()-i][startP.getY()] != 0){
-                   access = false;
-                   break;
-               }
-           }
-           if(access){
-               for (int i =0; i<num; i++){
-                   gameboard[startP.getX()-i][startP.getY()] = 1;
+            //           kontolujem smer dolava
+            for (int i =1; i<num; i++){
+                if (startP.getX()-i<0){
+                    access = false;
+                    break;
+                }
+                if(gameboard[startP.getX()-i][startP.getY()] != 0){
+                    access = false;
+                    break;
+                }
+            }
+            if(access){
+                for (int i =0; i<num; i++){
+                    gameboard[startP.getX()-i][startP.getY()] = 1;
 //                   boky
-                   if(startP.getY()-1 >=0){
-                       gameboard[startP.getX()-i][startP.getY()-1] = -1;
-                   }
-                   if(startP.getY()+1 <=9){
-                       gameboard[startP.getX()+i][startP.getY()-1] = -1;
-                   }
-               }
-               //                   hore dole
-               if(startP.getX()-1 >=0){
-                   gameboard[startP.getX()-1][startP.getY()] = -1;
-               }
-               if(startP.getX()+num <=9){
-                   gameboard[startP.getX()+num][startP.getY()] = -1;
-               }
-               return true;
-           }
-           access= true;
-       }
-       return false;
+                    border(startP.getX()-i,startP.getY()+1);
+                    border(startP.getX()-i,startP.getY()+1);
+                }
+                //                   hore dole
+                border(startP.getX()+1,startP.getY());
+                border(startP.getX()-num,startP.getY());
+                // diagonal
+                border(startP.getX()+1,startP.getY()+1);
+                border(startP.getX()-num,startP.getY()+1);
+                border(startP.getX()+1,startP.getY()-1);
+                border(startP.getX()-num,startP.getY()-1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void border(int x, int y){
+        if (x > 9 || x < 0 || y > 9 || y < 0){
+
+        } else {
+            gameboard[x][y] = -1;
+        }
     }
 
     public void printG(){
@@ -227,7 +202,7 @@ public class Ships {
                 if(gameboard[j][i]== -1){
                     gameboard[j][i]=0;
                 }
-                System.out.print(gameboard[j][i]+" ");
+                System.out.print(gameboard[j][i]+"  ");
             }
             System.out.println();
         }
